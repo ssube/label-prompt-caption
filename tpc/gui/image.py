7ea702e6572f3a26226c60bb42f1517d4ac333bd
Sean Sube <seansube@gmail.com>
2024-09-08 22:50:20 -0500

    fix captioning when image does not have any metadata

--- tpc/gui/image.py
+++ tpc/gui/image.py
@@ -40,7 +40,10 @@ def caption_image(group_meta: GroupMetaFile, image: str, model: str, prompt: str
 
     # apply group caption template
     caption_template = jinja.from_string(group_meta.group.caption)
-    caption_args = get_annotation_dict(group_meta.images[image])
+    caption_args = {}
+    if image in group_meta.images:
+        caption_args = get_annotation_dict(group_meta.images[image])
+
     caption = caption_template.render(**caption_args, caption=model_caption)
     return caption
 
