fbbf8e4a4342432d9232e9ff56d4a5de973f5cec
Sean Sube <seansube@gmail.com>
2024-09-08 20:23:04 -0500

    add app title

--- tpc/gui/app.py
+++ tpc/gui/app.py
@@ -4,9 +4,11 @@ from .dataset import make_dataset_tab
 from .group import make_group_tab
 from .image import make_image_tab
 
-with gr.Blocks() as app:
+with gr.Blocks(title="LPC Studio") as app:
     app_state = gr.State()
     group_state = gr.State()
+
+    gr.Markdown("# Label-Prompt-Caption Studio")
 
     with gr.Tab("Dataset"):
         make_dataset_tab(app_state)
